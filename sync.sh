4bf874950fdacfe1e0d3648e136e0e2e96709e83
dgonz <dgonz@qtxmail.com>
2022-11-08 00:48:12 -0400

    Adding lockfile

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # create a lock file and avoid running the same commands
-flock -n /tmp -c "rsync -aAvxh --delete ${SOURCE_FOLDER} ${DESTINATION_FOLDER} > ${DESTINATION_FOLDER}/latest-sync.log"
+flock -n /tmp/sync.lock -c "rsync -aAvxh --delete ${SOURCE_FOLDER} ${DESTINATION_FOLDER} > ${DESTINATION_FOLDER}/latest-sync.log"
